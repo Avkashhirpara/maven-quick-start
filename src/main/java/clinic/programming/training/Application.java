f9cad3d6de9fab83a8739a2ba4d00148cf38b002
Avkash Hirpara <avkashh@ecwatl.com>
2018-01-10 17:32:19 +0530

    added commons langs dependancy and used string utils

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,25 @@
 package clinic.programming.training;
 
+import org.apache.commons.lang3.StringUtils;
 public class Application {
     
+	
+	 public int countWords(String words){
+		 String[]  separateWords = StringUtils.split(words," ");
+		 return (separateWords == null ) ? 0 : separateWords.length;
+	 }
     public Application() {
         System.out.println ("Inside Application");
     }
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
+		
+		
     	System.out.println ("Starting Application");
 	Application app = new Application();
+	
+	int count = app.countWords ("i have many oworks");
+	System.out.println (count +"count of the words that is provided");
     }
 }
